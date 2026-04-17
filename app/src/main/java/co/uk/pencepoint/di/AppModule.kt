1f69f0159d5c05525669587c3e5f26c5a7562a30
gokulkesavalu <kvgokul.mns@gmail.com>
2026-04-17 11:52:32 +0100

    Remove unused `Provides` import in `AppModule.kt`

--- app/src/main/java/co/uk/pencepoint/di/AppModule.kt
+++ app/src/main/java/co/uk/pencepoint/di/AppModule.kt
@@ -4,7 +4,6 @@ import co.uk.pencepoint.data.repository.LocalTaxProvider
 import co.uk.pencepoint.domain.repository.TaxProvider
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
